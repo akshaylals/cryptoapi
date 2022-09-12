7f4b1e1a8044cd6609f33db9f26fced4f3f5a62d
akshaylals <akshaylal.s@inapp.com>
2022-09-12 12:50:55 +0530

    added different elliptic curves

--- apps/cert/utils.py
+++ apps/cert/utils.py
@@ -1,9 +1,32 @@
 import os
+from importlib import import_module
 
 from cryptography.hazmat.primitives import serialization
 
 from apps.config import CERTIFICATE_DIR
 
+
+curves = [
+    'SECP256R1',
+    'SECP384R1',
+    'SECP521R1',
+    'SECP224R1',
+    'SECP192R1',
+    'SECP256K1',
+    'BrainpoolP256R1',
+    'BrainpoolP384R1',
+    'BrainpoolP512R1',
+    'SECT571K1',
+    'SECT409K1',
+    'SECT283K1',
+    'SECT233K1',
+    'SECT163K1',
+    'SECT571R1',
+    'SECT409R1',
+    'SECT283R1',
+    'SECT233R1',
+    'SECT163R2',
+]
 
 
 def createRsa(filename, public_exponent, key_size):
@@ -126,4 +149,42 @@ def createEd448(filename):
 
 
 def createEC(filename, curve):
-    pass
+    from cryptography.hazmat.primitives.asymmetric.ec import generate_private_key
+    
+    if os.path.exists(os.path.join(CERTIFICATE_DIR, filename)) or os.path.exists(os.path.join(CERTIFICATE_DIR, filename + '.pub')):
+        return {'message': 'File already exists', 'status': 400}
+
+    if curve not in curves:
+        return {'message': 'Invalid or Unsupported curve', 'curves': curves, 'status': 400}
+
+    c = getattr(import_module('cryptography.hazmat.primitives.asymmetric.ec'), curve)
+    private_key = generate_private_key(c)
+
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    with open(os.path.join(CERTIFICATE_DIR, filename), "wb") as f:
+        f.write(pem)
+    
+    public_key = private_key.public_key()
+
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    with open(os.path.join(CERTIFICATE_DIR, filename + '.pub'), "wb") as f:
+        f.write(pem)
+    
+    return {
+        'message': 'success',
+        'certificate': {
+            'curve': curve,
+            'private_key_file': filename,
+            'public_key_file': filename + '.pub'
+        },
+        'status': 200
+    }
